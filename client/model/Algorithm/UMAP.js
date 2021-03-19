cbfda5a8c5c7b3faf39e232fc7206cff0b40cc5c
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-03-19 18:18:34 +0100

    rimossi moduli trasformati in classi

---
+++ client/model/Algorithm/UMAP.js
@@ -0,0 +1,15 @@
+const Algorithm = require('./Algorithm');
+const druid = require('@saehrimnir/druidjs');
+
+class UMAP extends Algorithm.Algorithm {
+
+    constructor() {
+        super();
+    }
+
+    setAlgorithm(data, param) {
+        return new druid.UMAP(data, param.neighbors, 1, 1, param.dims);
+    }
+}
+
+exports.UMAP = UMAP;
