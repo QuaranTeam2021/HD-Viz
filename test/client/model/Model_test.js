f734cf046ba1c6604639b4a7807d344010e4c027
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-03-20 19:50:09 +0100

    implementati test

--- test/client/model/Model_test.js
+++ test/client/model/Model_test.js
@@ -2,7 +2,7 @@ const {Model} = require('../../../client/model/Model');
 const {expect} = require('chai');
 const {GraphState} = require('../../../client/model/GraphState');
 const {Data} = require('../../../client/model/Data');
-const {ConcreteGraph} = require('../../../client/model/Graph');
+const {ConcreteGraph, Graph} = require('../../../client/model/Graph');
 
 describe('Testing model class', function() {
 
@@ -35,7 +35,7 @@ describe('Testing model class', function() {
             const graphState = new GraphState(new ConcreteGraph([], 'prova'), 'LLE', []);
             mod.pushGraphState(graphState);
 
-            return mod.getGraphsNumber == 1;
+            return mod.getGraphsNumber() === 1;
         })
 
         it('Must push GraphState in graphs', function() {
@@ -55,7 +55,7 @@ describe('Testing model class', function() {
             mod.pushGraphState(graphState1);
             mod.pushGraphState(graphState2);
 
-            expect(mod.getGraphState()).to.deep.equal(graphState2)
+            expect(mod.getGraphState()).to.deep.equal(graphState2);
         })
     }) 
 
@@ -166,7 +166,7 @@ describe('Testing model class', function() {
             const mod = new Model([1, 2, 3], [graphState, graphState1, graphState2]);
             mod.reset();
 
-            return mod.getGraphsNumber == 0;
+            return mod.getGraphsNumber() == 0;
         })
 
         it('Must remove GraphState from graphs', function() {
@@ -189,6 +189,117 @@ describe('Testing model class', function() {
             const mod = new Model([1, 2, 3], [graphState, graphState1, graphState2]);
 
             expect(mod.getGraphs()).to.deep.equal([graphState, graphState1, graphState2]);
+        })
+
+        it('Must return empty array', function() {
+            const mod = new Model([1, 2, 3], []);
+
+            expect(mod.getGraphs()).to.deep.equal([]);
+        })
+    })
+
+    context('Testing getGraphAtIndex', function() {
+
+        it('Must return a graph object', function() {
+            const ConcreteGraph1 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const ConcreteGraph2 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const GraphState1 = new GraphState(ConcreteGraph1, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            const GraphState2 = new GraphState(ConcreteGraph2, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            let graphs = [];
+            graphs.push(GraphState1); graphs.push(GraphState2);
+            const mod = new Model([], graphs);
+            const result = mod.getGraphAtIndex(0);
+
+            return typeof result === Graph;
+        })
+
+        it('Must return the correct graph', function() {
+            const ConcreteGraph1 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const ConcreteGraph2 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const GraphState1 = new GraphState(ConcreteGraph1, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            const GraphState2 = new GraphState(ConcreteGraph2, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            let graphs = [];
+            graphs.push(GraphState1); graphs.push(GraphState2);
+            const mod = new Model([], graphs);
+            const result = mod.getGraphAtIndex(1);
+
+            expect(result).to.deep.equal(ConcreteGraph2);
+        })
+    })
+
+    context('Testing getAlgorithmAtIndex', function() {
+
+        it('Must return a string', function() {
+            const ConcreteGraph1 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const ConcreteGraph2 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const GraphState1 = new GraphState(ConcreteGraph1, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            const GraphState2 = new GraphState(ConcreteGraph2, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            let graphs = [];
+            graphs.push(GraphState1); graphs.push(GraphState2);
+            const mod = new Model([], graphs);
+            const result = mod.getAlgorithmAtIndex(0);
+
+            return typeof result === String;
+        })
+
+        it('Must return the correct algorithm', function() {
+            const ConcreteGraph1 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const ConcreteGraph2 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const GraphState1 = new GraphState(ConcreteGraph1, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            const GraphState2 = new GraphState(ConcreteGraph2, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            let graphs = [];
+            graphs.push(GraphState1); graphs.push(GraphState2);
+            const mod = new Model([], graphs);
+            const result = mod.getAlgorithmAtIndex(1);
+
+            expect(result).to.deep.equal('PCA');
+        })
+    })
+
+    context('Testing getSelectedFeatureAtIndex', function() {
+
+        it('Must return an array', function() {
+            const ConcreteGraph1 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const ConcreteGraph2 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const GraphState1 = new GraphState(ConcreteGraph1, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            const GraphState2 = new GraphState(ConcreteGraph2, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            let graphs = [];
+            graphs.push(GraphState1); graphs.push(GraphState2);
+            const mod = new Model([], graphs);
+            const result = mod.getSelectedFeaturesAtIndex(0);
+
+            return typeof result === Array;
+        })
+
+        it('Must return the correct array', function() {
+            const ConcreteGraph1 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const ConcreteGraph2 = new ConcreteGraph([1, 2, 3, 4, 5], 'prova');
+            const GraphState1 = new GraphState(ConcreteGraph1, 'PCA', ['id', 'id1', 'id2', 'id3', 'id4']);
+            const GraphState2 = new GraphState(ConcreteGraph2, 'PCA', ['id3', 'id4', 'id5', 'id6', 'id7']);
+            let graphs = [];
+            graphs.push(GraphState1); graphs.push(GraphState2);
+            const mod = new Model([], graphs);
+            const result = mod.getSelectedFeaturesAtIndex(1);
+
+            expect(result).to.deep.equal(['id3', 'id4', 'id5', 'id6', 'id7']);
+        })
+    })
+
+    context('Testing getGraphsNumber', function() {
+
+        it('Must return the correct graphs number', function() {
+            const graphState = new GraphState(new ConcreteGraph([], 'prova'), 'LLE', []);
+            const graphState1 = new GraphState(new ConcreteGraph([], 'prova'), 'LLE', []);
+            const graphState2 = new GraphState(new ConcreteGraph([], 'prova'), 'LLE', []);
+            const mod = new Model([1, 2, 3], [graphState, graphState1, graphState2]);
+
+            expect(mod.getGraphsNumber()).to.equal(3);
+        })
+
+        it('Must return zero', function() {
+            const mod = new Model([1, 2, 3], []);
+
+            expect(mod.getGraphsNumber()).to.equal(0);
         })
     })
 })
