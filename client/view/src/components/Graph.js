02d18d174a00696de611fc3904d2d467221bf38a
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-03-20 15:59:03 +0100

    spostata view in client

---
+++ client/view/src/components/Graph.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function Graph({ svg, onDelete, index }) {
+	const [i] = useState(index);
+	
+	const createMarkup = () => {
+		return {__html: svg}
+	}
+	return (
+		<div>
+			<button onClick={() => onDelete(i)}>RIMUOVI</button>
+			<div dangerouslySetInnerHTML={createMarkup()}></div>
+		</div>
+	)
+}
+export default Graph; 
