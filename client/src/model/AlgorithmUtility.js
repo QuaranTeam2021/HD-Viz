04e5a655615efcf7571672ebe242f1a8a6053491
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-03-27 11:20:35 +0100

    aggiunto file utility per importazione algoritmi

---
+++ client/src/model/AlgorithmUtility.js
@@ -0,0 +1,13 @@
+const {PCA} = require('./Algorithm/PCA');
+const {UMAP} = require('./Algorithm/UMAP');
+const {FASTMAP} = require('./Algorithm/FASTMAP');
+const {ISOMAP} = require('./Algorithm/ISOMAP');
+const {TSNE} = require('./Algorithm/TSNE');
+const {LLE} = require('./Algorithm/LLE');
+
+exports.PCA = PCA;
+exports.UMAP = UMAP;
+exports.FASTMAP = FASTMAP;
+exports.ISOMAP = ISOMAP;
+exports.TSNE = TSNE;
+exports.LLE = LLE;
