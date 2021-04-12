f7048231bcbf43d0b4c0b74277b5291427ecbeb1
alicegibe <alice.gibellato@studenti.unipd.it>
2021-04-12 12:21:08 +0200

    Update FASTMAPfeatures.js

--- client/src/view/components/FASTMAPfeatures.js
+++ client/src/view/components/FASTMAPfeatures.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SelectDimensione from './SelectDimensione';
+import SliderSize from './SliderSize';
 
 export default function FASTMAP() {
     
     return (
         <div>
         <SelectDimensione/>
+        <SliderSize/>
         </div>
     );
   }
