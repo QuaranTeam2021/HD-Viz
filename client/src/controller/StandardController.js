c3a1c631dd66f64192d4454cef7a0aee63ea7463
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-04-24 15:02:42 +0200

    corretto errore

--- client/src/controller/StandardController.js
+++ client/src/controller/StandardController.js
@@ -5,7 +5,6 @@ export default class StandardController extends StandardGraphController {
 
     createGraph(graphId, type, features) {
         let data = this.store.calculateSelectedData(features);
-        data = data.slice(1);
         let graph = new StandardGraph(graphId, type, data);
         this.store.addGraph(graph);
     }
