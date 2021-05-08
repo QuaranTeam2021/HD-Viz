506e8b623bb031be34f650b2cc714d9367ed09c6
Dam-99 <57217353+Dam-99@users.noreply.github.com>
2021-05-08 18:00:12 +0200

    Cancellazione dataset funzionante

--- client/src/view/components/database/Database.js
+++ client/src/view/components/database/Database.js
@@ -7,76 +7,81 @@ import DeleteDb from './DeleteDb';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextFieldAddDb from './TextFieldAddDb';
 
+const controllerManager = new DatabaseManagerController();
+
 export default function Database() {
-    const controllerManager = new DatabaseManagerController();
-    const [datasets] = useState(['tabella', 'tabella2', 'tabella3', 'tabella4', 'tabella5']); // <--controllerManager.getTablesName();
+    const [datasets, setDatasets] = useState(['tabella', 'tabella2', 'tabella3', 'tabella4', 'tabella5']); // <--controllerManager.getTablesName();
     const [tableName, setTableName] = useState('');
     const [insertDs, setInsertDs] = useState([]);
-    const [deleteDs, setDeleteDs] = useState([]);
     const [disableName, setDisableName] = useState(true);
     const [name, setName] = useState("");
     const [sentDataset, setDs] = useState([]);
 
     const onChangeTableName = e => {
         setTableName(e.target.value);
-    }
+    };
 
     const onChangeInsertDs = e => {
-      let v = e.target.files[0];  
-      setInsertDs(prev => {
+        let v = e.target.files[0];
+        setInsertDs(prev => {
             return v === undefined ? prev : v;
-        })  
+        });
         setName(() => {
-          return v === undefined ? insertDs.name : v.name;
-      })
-      setDisableName(v === undefined);
-      controllerManager.upload(tableName ? tableName : insertDs.name, insertDs);
+            return v === undefined ? insertDs.name : v.name;
+        });
+        setDisableName(v === undefined);
+        // insertDs non é aggiornato quando viene chiamata la funzione
+        controllerManager.upload(tableName ? tableName : insertDs.name, insertDs);
     };
 
     const onClickDelete = idx => {
-        console.log('click');
-        console.log(idx);
-        controllerManager.deleteTable(deleteDs);
-        setDeleteDs(list => list.filter((_d, i) => i !== idx))
-    }; 
+        let deletedTable = "";
+        setDatasets(list => list.filter((d, i) => {
+            if (i === idx)
+                deletedTable = d;
+            return i !== idx;
+        }));
+
+        if (deletedTable !== "")
+            controllerManager.deleteTable(deletedTable);
+    };
 
     const onChangeName = e => {
         setName(e.target.value);
-      }; 
+    };
 
-      const optionsAddDs = useCallback(
+    const optionsAddDs = useCallback(
         () => {
-          let select; 
-          select = name !== "";
-          setDs(select);
+            let select;
+            select = name !== "";
+            setDs(select);
         },
         [name]
-        );  
-    
-     useEffect(() => {
+    );
+
+    useEffect(() => {
         optionsAddDs();
-    }, [optionsAddDs]);  
- 
+    }, [optionsAddDs]);
+
     const onClickDs = action(() => {
         let formData = {
-          name 
-        }; 
-        formData.name = name; 
-      });
-      
+            name
+        };
+        formData.name = name;
+    });
+
 
     return (
         <div>
             <ButtonAddDb onChange={onChangeInsertDs} onChangeTableName={onChangeTableName} />
-            {console.log(insertDs.name)}
-            <TextFieldAddDb onChangeName={onChangeName} fileName={insertDs.name} nameDs={name} disabled={disableName}/>
-            <ButtonConfirmAddDb onChange={onClickDs} disabled={!sentDataset} fileName={insertDs.name}/>
+            <TextFieldAddDb onChangeName={onChangeName} fileName={insertDs.name} nameDs={name} disabled={disableName} />
+            <ButtonConfirmAddDb onChange={onClickDs} disabled={!sentDataset} fileName={insertDs.name} />
             <div id="dataset">
-            <>
-            {datasets.map((d, i) => <FormControlLabel key={i} control={<DeleteDb onClick={onClickDelete} />} label={d} value={d} />)}
-             </>
+                <>
+                    {datasets.map((d, i) => <FormControlLabel key={i} control={<DeleteDb onClickDelete={onClickDelete} idx={i} />} label={d} value={d} />)}
+                </>
             </div>
-                
-         </div>
+
+        </div>
     );
 }
