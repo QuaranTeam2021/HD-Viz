9f4dcaa23f98bc73bcdc7c69007f5ffa41e51abb
MatteoSinigaglia <matteo.sinigaglia99@gmail.com>
2021-06-09 18:57:11 +0200

    aggiunti test

--- client/src/view/components/SCPMOptions.js
+++ client/src/view/components/SCPMOptions.js
@@ -1,12 +1,10 @@
 import React, { useCallback, useEffect } from 'react';
 import SelectVizColumns from './database/SelectVizColumns';
 
-
 export default function SCPMOptions({ graphViz, buttonRef, currentOptions, setCurrentOptions, setDisabled }) {
 
   const [allCols, setAllCols] = React.useState([]);
   const [selected, setSelected] = React.useState([]);
-
 
   const commitChanges = useCallback(() => {
     if (currentOptions.oldSel !== selected)
